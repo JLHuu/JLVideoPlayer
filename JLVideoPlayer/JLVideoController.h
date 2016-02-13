1796b825fe7688e9b5e1ae842d59d44238a3872f
hujiele <hujiele@hujieledeMacBook-Pro.local>
2016-02-13 23:25:14 +0800

    第二次修改

---
+++ JLVideoPlayer/JLVideoController.h
@@ -0,0 +1,13 @@
+//
+//  JLVideoController.h
+//  JLVideoPlayer
+//
+//  Created by hujiele on 16/2/13.
+//  Copyright © 2016年 JLHuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLVideoController : UIViewController
+
+@end
